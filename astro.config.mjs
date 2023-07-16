6ed64b9946ed6ba83355a3555c5e024fd4e1bbf9
Loy Yee Ko <koloyyee@gmail.com>
2023-07-16 11:28:36 +0800

    update: deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
 	integrations: [tailwind({ applyBaseStyles: false })],
 	site: 'https://koloyyee.github.io',
+	base: 'blog',
 })
