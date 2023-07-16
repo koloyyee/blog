e49c4cc6bc32d74686422d8b3c45e91752fdadf7
Loy Yee Ko <koloyyee@gmail.com>
2023-07-16 12:05:07 +0800

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
+import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind({ applyBaseStyles: false })],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 	site: 'https://koloyyee.github.io',
-	base: 'blog',
+	output: 'static',
+	adapter: vercel(),
 })
